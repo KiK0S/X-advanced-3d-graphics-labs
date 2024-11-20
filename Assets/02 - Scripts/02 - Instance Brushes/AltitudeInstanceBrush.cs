f65f3a27d476d98dbf88fcf6d89a139c06488073
arzhantsev <arzhantsev64@yandex.ru>
2024-11-20 16:03:37 +0100

    Added 2 new instance brushes

---
+++ Assets/02 - Scripts/02 - Instance Brushes/AltitudeInstanceBrush.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltitudeInstanceBrush : InstanceBrush {
+    public int n_max_entities = 10;
+    public float heightThreshold = 15;
+
+    public override void draw(float x, float z) {
+        for (int i = 0; i < n_max_entities; ++i) {
+            float new_x = x + Random.Range(-radius, radius);
+            float new_z = z + Random.Range(-radius, radius);
+            if (terrain.getInterp(new_x, new_z) > heightThreshold) {
+                spawnObject(new_x, new_z);
+            }
+        }
+    }   
+}
---
+++ Assets/02 - Scripts/02 - Instance Brushes/SteepnessInstanceBrush.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteepnessInstanceBrush : InstanceBrush {
+    public int n_max_entities = 10;
+    public float steepnessThreshold = 1;
+
+    public override void draw(float x, float z) {
+
+        for (int i = 0; i < n_max_entities; ++i) {
+            float new_x = x + Random.Range(-radius, radius);
+            float new_z = z + Random.Range(-radius, radius);
+            if (terrain.getSteepness(new_x, new_z) <= steepnessThreshold) {
+                spawnObject(new_x, new_z);
+            }
+        }
+    }   
+}
